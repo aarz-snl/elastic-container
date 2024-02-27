c326d68b849b32a27084d871c1364a1694cba35d
Andrew Arz <149685528+aarz-snl@users.noreply.github.com>
2024-02-27 12:00:50 -0500

    Update enrollment_token.sh

--- enrollment_token.sh
+++ enrollment_token.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# Load environment variables
-. .env
+# Load environment variables from .env file
+if [ -f .env ]; then
+    source .env
+else
+    echo ".env file not found"
+    exit 1
+fi
 
 # Define common headers
 HEADERS=(
